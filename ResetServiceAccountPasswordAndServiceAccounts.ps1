598ee4f2232152e3dac384e6e0fabf71206356a9
unknown <astrahsburg@D3WV4Q32.specialized.com>
2015-07-14 10:38:55 -0700

    Signed-off-by: unknown <astrahsburg@D3WV4Q32.specialized.com>

--- ResetServiceAccountPasswordAndServiceAccounts.ps1
+++ ResetServiceAccountPasswordAndServiceAccounts.ps1
@@ -3,7 +3,9 @@
     [string]$ServiceAccount="",  #ServiceAccount In Local AD to change password for (username only no domain)
     [string]$Password="",  #New password to assign
     [string[]]$Hosts,  #List of hosts to search for services running under $ServiceAccount
-    [switch]$ResetPassword
+    [string]$ADGroupToSearch="",
+    [switch]$ResetPassword,
+    [switch]$Restart
 )
 
 IF ($ServiceAccount.Length -lt 1)
@@ -12,21 +14,22 @@ IF ($ServiceAccount.Length -lt 1)
 }
 $ServiceAccount = $ServiceAccount -replace "\\", "\\";
 
-IF ($Hosts.Count -eq 0)
+IF ($Hosts.Count -eq 0 -and $ADGroupToSearch.Length -eq 0)
 {
     $Hosts = (Read-Host -Prompt "Provide Host(s) Name");
 }
 
-#Reset AD Password
 IF ($Password.Length -lt 1)
 {
     $SecurePassword = (Read-Host -Prompt "Provide New Password" -AsSecureString);
 
     $BSTR = [system.runtime.interopservices.marshal]::SecureStringToBSTR($SecurePassword)
     $Password = [system.runtime.interopservices.marshal]::PtrToStringAuto($BSTR)
+} else {
+    $SecurePassword = ConvertTo-SecureString $Password -AsPlainText -Force;
+    $BSTR = [system.runtime.interopservices.marshal]::SecureStringToBSTR($SecurePassword)
+    $Password = [system.runtime.interopservices.marshal]::PtrToStringAuto($BSTR)
 }
-
-
 
 IF ($ResetPassword)
 {
@@ -53,31 +56,65 @@ Try
                 write-host "Service account update failed" -ForegroundColor "Red";
             }
             
-            if ($Service.StopService().ReturnValue -eq 0)
+            if (-not $Restart)
             {
-                write-host "Service stopped" -ForegroundColor "Green";
-            } else {
-                write-host "Service failed to stop" -ForegroundColor "Yellow";
-            }
+                $PSService = get-service -ComputerName $hostname -Name $serviceName;
+                if ($PSService.Status -eq "Started")
+                {
+                    $PSService.Stop();
+                }
+                try
+                {
+                    $PSService.WaitForStatus("Stopped", '00:01:00');
+                }
+                catch
+                {}
+                if ($PSService.Status -eq "Stopped")
+                {
+                    write-host "Service stopped" -ForegroundColor "Green";
+                } else {
+                    write-host "Service failed to stop" -ForegroundColor "Yellow";
+                }
 
-            if ($Service.StartService().ReturnValue -eq 0)
-            {
-                write-host "Service Started Successfully" -ForegroundColor "Green";
-            } else {
-                write-host "Service did not restart" -ForegroundColor "Green";
+                if ($PSService.Status -eq "Stopped")
+                {
+                    $PSService.Start();
+                }
+                try
+                {
+                    $PSService.WaitForStatus("Started", '00:01:00');
+                }
+                catch
+                {}
+                if ($PSService.Status -eq "Started")
+                {
+                    write-host "Service Started Successfully" -ForegroundColor "Green";
+                } else {
+                    write-host "Service did not restart" -ForegroundColor "Green";
+                }
+                Get-WmiObject -Class Win32_Service -ComputerName $hostname -Filter "Name = '$serviceName'" | Select Name, StartName, State;
+                Write-Host "";
             }
-            Get-WmiObject -Class Win32_Service -ComputerName $hostname -Filter "Name = '$serviceName'" | Select Name, StartName, State;
+        }
+        Write-Host "";
+        Get-WmiObject -Class Win32_Service -ComputerName $hostname -Filter "StartName Like '$ServiceAccount@%' OR StartName Like '%\\$ServiceAccount'"  | Select Name, StartName, State;
+        if ($Restart)
+        {
+            Write-Host "Restarting $hostname";
+            Restart-Computer $hostname -Force;
+			Write-Host "";
+			Write-Host "";
         }
     }
 }
 Catch
 {
     [system.exception];
-    "system exception caught";
+    "system exception caught $_.InvocationInfo.ScriptLineNumber $_.Exception.ItemName $_.Exception.Message";
 }
 Finally
 {
     
     [Runtime.InteropServices.Marshal]::ZeroFreeBSTR($BSTR);
-    Remove-Variable Password,SecurePassword, BSTR;
+    Remove-Variable Password,SecurePassword, BSTR, Hosts, ServiceAccount;
 }
---
+++ SearchServersForServiceAccount.ps1
@@ -0,0 +1,71 @@
+﻿param
+(
+    [string]$ADGroup,
+    [string]$Hostname,
+    [string]$ADOSFilter,
+    [string]$ServiceAccount,
+    [switch]$ServerListOnly
+)
+
+$__hosts = @();
+
+if ($Hostname.Length -gt 0)
+{
+    $__hosts += $Hostname;
+}
+
+if ($ADOSFilter.Length -gt 0)
+{
+    $_hosts = @(Get-ADComputer -Filter { OperatingSystem -Like '*Windows Server*'} | Where-Object {$_.DistinguishedName -notlike "*OU=Hosted*"} | Select Name);
+    foreach ($_host in $_hosts)
+    {
+        $__hosts += $_host.Name;
+    }
+}
+
+
+if ($ADGroup.Length -gt 0)
+{
+    foreach ($_host in get-adGroupMember "$ADGroup")
+    {
+        $__hosts += $_host.Name;
+    }
+}
+
+"" | Out-File "./test.txt"
+
+foreach ($hostname in $__hosts)
+{
+    $services = @();
+    if (-not $ServerListOnly)
+    {
+        try
+        {
+            if (Test-Connection $hostname -Count 1 -Quiet)
+            {
+                $services = Get-WmiObject -Class Win32_Service -ComputerName $hostname -Filter "StartName Like '$ServiceAccount@%' OR StartName Like '%\\$ServiceAccount'";
+            }
+        } catch {}
+
+        Write-Host ;
+        Write-Host $_host;
+        Write-Host "-----------------";
+        foreach ($service in $services)
+        {
+            Write-Host $service.Name $service.Status;
+        }
+    } else {
+        try
+        {
+            if (Test-Connection $hostname -Count 1 -Quiet)
+            {
+                $services = @(Get-WmiObject -Class Win32_service -ComputerName $hostname -Filter "StartName Like '$ServiceAccount@%' OR StartName Like '%\\$ServiceAccount'");
+            }
+        } catch {}
+        if ($services.count -gt 0)
+        {
+            Write-Host $hostname;
+            $hostname | Out-File "./test.txt" -Append
+        }
+    }
+}
